26b8236ef600b81f17231108831d7eb80e4cdcea
Lisy051 <2847797925@qq.com>
2024-03-17 23:32:12 +0800

    【修改】适配AURORA F4PLUS

---
+++ src/main/target/AURORAF4PLUS/target.c
@@ -0,0 +1,45 @@
+/*
+ * This file is part of INAV Project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Alternatively, the contents of this file may be used under the terms
+ * of the GNU General Public License Version 3, as described below:
+ *
+ * This file is free software: you may copy, redistribute and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+
+#include <stdint.h>
+
+#include "platform.h"
+
+#include "drivers/bus.h"
+#include "drivers/io.h"
+#include "drivers/pwm_mapping.h"
+#include "drivers/timer.h"
+#include "drivers/pinio.h"
+#include "drivers/sensor.h"
+
+timerHardware_t timerHardware[] = {
+    DEF_TIM(TIM2, CH4,  PA3,   TIM_USE_OUTPUT_AUTO ,    0, 0), // S1
+    DEF_TIM(TIM3, CH3,  PB0,   TIM_USE_OUTPUT_AUTO ,    0, 0), // S2
+    DEF_TIM(TIM2, CH3,  PA2,   TIM_USE_OUTPUT_AUTO ,    0, 0), // S3
+    DEF_TIM(TIM3, CH4,  PB1,   TIM_USE_OUTPUT_AUTO ,    0, 0), // S4
+
+    DEF_TIM(TIM4, CH3,  PB8,    TIM_USE_LED,         0, 0), // LED_STRIP
+};
+
+const int timerHardwareCount = sizeof(timerHardware) / sizeof(timerHardware[0]);
